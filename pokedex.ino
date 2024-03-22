5e59175cf732ca1632e685f5d72e297e54b578e7
Jotunn9025 <youhanlalwani@gmail.com>
2024-03-22 15:39:15 +0530

    first

---
+++ pokedex.ino
@@ -0,0 +1,153 @@
+#include "esp_camera.h"
+#include <Arduino.h>
+#include "Base64.h"
+#include <WiFi.h>
+
+#define CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#define CAPTURE_INTERVAL 60000
+
+const char* ssid = "name";
+const char* password = "password";
+
+void custom_base64_encode(char *output, const char *input, int inputLen) {
+  const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  int i = 0, j = 0;
+  unsigned char char_array_3[3], char_array_4[4];
+
+  while (inputLen--) {
+    char_array_3[i++] = *(input++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for(i = 0; (i <4) ; i++)
+        output[j++] = base64_chars[char_array_4[i]];
+      i = 0;
+    }
+  }
+
+  if (i) {
+    for(j = i; j < 3; j++)
+      char_array_3[j] = '\0';
+
+    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+    for (j = 0; (j < i + 1); j++)
+      output[j] = base64_chars[char_array_4[j]];
+
+    while((i++ < 3))
+      output[j++] = '=';
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("connecting to wifi");
+  }
+  Serial.println("connected to wifi");
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  captureLoop();
+}
+
+size_t calcBase64EncLen(size_t len) {
+  return 4 * ((len + 2) / 3);
+}
+
+void captureLoop() {
+  unsigned long previousMillis = 0;
+  Serial.println("Inside captureLoop");
+  while (1) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= CAPTURE_INTERVAL) {
+      previousMillis = currentMillis;
+
+      camera_fb_t *fb = esp_camera_fb_get();
+      if (!fb) {
+        continue;
+      }
+
+      size_t encodedSize = calcBase64EncLen(fb->len);
+      char *encodedImage = (char *)malloc(encodedSize);
+      if (!encodedImage) {
+        free(fb->buf);
+        esp_camera_fb_return(fb);
+        continue;
+      }
+      custom_base64_encode(encodedImage, (char *)fb->buf, fb->len);
+
+      Serial.println("Base64 Encoded Image:");
+      Serial.println(encodedImage);
+
+      free(encodedImage);
+      free(fb->buf);
+      esp_camera_fb_return(fb);
+    }
+  }
+}
+
+void loop() {
+}
